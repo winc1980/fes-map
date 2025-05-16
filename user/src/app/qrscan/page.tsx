8053e3c8298fa7b08e469437f8d90619f9185d99
ichiro <ichi1.9adgjm@gmail.com>
2025-05-16 15:44:49 +0900

    add shadocn

---
+++ user/src/app/qrscan/page.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from "react";
+import { Box } from "@chakra-ui/react";
+
+const ScanQrPage: FC = () => {
+    return (
+        <Box>
+        <h1>ScanQr Page</h1>
+        <p>This is the ScanQr page content.</p>
+        </Box>
+    );
+}
+export default ScanQrPage;
---
+++ user/src/compontents/header.tsx
@@ -0,0 +1,8 @@
+import React, { FC } from "react";
+
+
+const Header: FC = () => {
+  return (
+  );
+};
+export default Header;
