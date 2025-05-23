cfd4fc6e647f878cdfa34835cf71f45f1511a066
ichiro <ichi1.9adgjm@gmail.com>
2025-05-23 19:48:00 +0900

    setup firebase firebase初期設定 #13

--- user/src/service/firebase.ts
+++ user/src/service/firebase.ts
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDa9FnjTdGNssShhAdgZpDnnsOXAGvVthA",
-  authDomain: "circle-app-project.firebaseapp.com",
-  projectId: "circle-app-project",
-  storageBucket: "circle-app-project.firebasestorage.app",
-  messagingSenderId: "684667399496",
-  appId: "1:684667399496:web:076672928dff10cf71d77d",
-  measurementId: "G-5R6GLXR2W6"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
